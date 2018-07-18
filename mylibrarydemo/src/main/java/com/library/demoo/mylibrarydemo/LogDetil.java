33baef5bf6dc79aff62e632cc877d513f01b335f
Rajesh <rajesh@speedkyat.com>
2018-07-18 13:30:50 +0630

    hi fdfs

---
+++ mylibrarydemo/src/main/java/com/library/demoo/mylibrarydemo/LogDetil.java
@@ -0,0 +1,16 @@
+package com.library.demoo.mylibrarydemo;
+
+import android.util.Log;
+
+/**
+ * Created by super on 7/18/18.
+ */
+
+public class LogDetil {
+
+    public void data(String msg)
+    {
+        Log.d("Tagg",msg);
+
+    }
+}
